cbeccd8f0b2652c03fe9c3174e84c9c8fde10c04
gjwehnes <gjwehnes@shaw.ca>
2021-10-11 13:45:25 -0600

    removed further imports from JUnit4

--- src/ex049/TwoDArrayPractice6Test.java
+++ src/ex049/TwoDArrayPractice6Test.java
@@ -1,8 +1,7 @@
 package ex049;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TwoDArrayPractice6Test {
 
