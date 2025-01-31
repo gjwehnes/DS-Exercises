869c012dfa087abfc2edd6cd48fe0d044fccfce3
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-01-31 14:39:31 -0700

    ex041 updated

--- ex041/TwoDArrayPractice1.java
+++ ex041/TwoDArrayPractice1.java
@@ -1,19 +1,7 @@
-
-/*
-* Complete the method doubleArr method so that it doubles the values of every element in the 2D array passed in.
-* Nothing should be returned - the array should be modified by the method.
-*/
 public class TwoDArrayPractice1 {
 
-	
 	public static void doubleArray(double[][] a) {
-	    
-	    for (int i = 0; i < a.length; ++i) {
-	     
-	        for (int j = 0; j < a[i].length; ++j) {
-	            
-		                a[i][j] = a[i][j] * 2;
-		            }
-		        }
-		    }
+		// add code here
 	}
+	
+}
