f490bc3f00bc8079fdc9b8711784366863aa8cbf
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-01-31 14:02:00 -0700

    ex037 updated

--- ex038/ArrayLists2.java
+++ ex038/ArrayLists2.java
@@ -1,12 +1,3 @@
-
-/*
-Create a method that:
-is called timesTwo
-returns nothing
-takes in a single parameter - an ArrayList of Integers called nums
-
-This method should take the ArrayList parameter and multiply every value by two.
-*/
 import java.util.ArrayList;
 
 public class ArrayLists2 {
